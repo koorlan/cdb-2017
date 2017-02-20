3361cdadc41518f90888296d1b991a06d12bcf3c
Korlan Colas <kcolas@excilys.com>
2017-02-20 14:56:40 +0100

    Company Dao: getId

--- src/com/formation/cdb/persistence/impl/CompanyDaoImpl.java
+++ src/com/formation/cdb/persistence/impl/CompanyDaoImpl.java
@@ -1,16 +1,21 @@
 package com.formation.cdb.persistence.impl;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import com.formation.cdb.mapper.impl.CompanyRowMapper;
 import com.formation.cdb.model.Company;
 
 public class CompanyDaoImpl implements com.formation.cdb.persistence.CompanyDao {
 
 	private Connection conn;
+	private CompanyRowMapper companyRowMapper;
 	
 	public CompanyDaoImpl(Connection conn){
 		this.conn = conn;
+		this.companyRowMapper = new CompanyRowMapper();
 	}
 	
 	@Override
@@ -31,9 +36,20 @@ public class CompanyDaoImpl implements com.formation.cdb.persistence.CompanyDao 
 		
 	}
 
+	private static final String GET_ID = "SELECT * FROM company WHERE id=";
+	
 	@Override
 	public Company get(long id) {
-		// TODO Auto-generated method stub
+		Company company;
+		
+		try {
+			ResultSet rs = conn.createStatement().executeQuery(GET_ID+Long.toString(id));
+			company = companyRowMapper.mapRow(rs);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 
