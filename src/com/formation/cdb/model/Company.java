16f95843c98943b4a627f4758ba55f1da57166fa
Korlan Colas <kcolas@excilys.com>
2017-02-20 11:03:22 +0100

    First write of company model

--- src/com/formation/cdb/model/Company.java
+++ src/com/formation/cdb/model/Company.java
@@ -1,5 +1,18 @@
 package com.formation.cdb.model;
 
 public class Company {
+	private long id;
+	private String name;
 
+	/**
+	 * Getters
+	 */
+	public long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	
 }
