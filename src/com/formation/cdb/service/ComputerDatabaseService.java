3a3dc0dfb37baea3fcc030c00b36a265f54d0464
Korlan Colas <kcolas@excilys.com>
2017-02-20 11:08:36 +0100

    Singleton of ComputerDatabaseService

--- src/com/formation/cdb/service/ComputerDatabaseService.java
+++ src/com/formation/cdb/service/ComputerDatabaseService.java
@@ -1,5 +1,15 @@
 package com.formation.cdb.service;
 
 public class ComputerDatabaseService {
+	private static ComputerDatabaseService INSTANCE;
+	
+	public ComputerDatabaseService(){
+	}
 
+	public static ComputerDatabaseService getInstance(){
+		if(INSTANCE == null){
+			INSTANCE = new ComputerDatabaseService();
+		}
+		return INSTANCE;
+	}
 }
