87fe35bacc6d28f5a05ee9eb0eaf7823ad0d830a
Korlan Colas <kcolas@excilys.com>
2017-02-20 11:01:25 +0100

    First write of computer model

--- src/com/formation/cdb/model/Computer.java
+++ src/com/formation/cdb/model/Computer.java
@@ -1,5 +1,35 @@
 package com.formation.cdb.model;
 
-public class Computer {
+import java.util.Date;
 
+public class Computer {
+	
+	//BIGINT on SQL DB
+	private long id;
+	private String name;
+	private Date introduced;
+	private Date discontinued;
+	private Company company;
+
+	/**
+	 * Getters
+	 */
+
+	public long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public Date getIntroduced() {
+		return introduced;
+	}
+	public Date getDiscontinued() {
+		return discontinued;
+	}
+	public Company getCompany() {
+		return company;
+	}
+	
+	
 }
