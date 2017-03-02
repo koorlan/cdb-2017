8cc8046fcc64ce102f01f4059f8ac6b7a4d382bc
Korlan Colas <kcolas@excilys.com>
2017-03-02 16:23:48 +0100

    sonar plugin + builder company dto

--- src/main/java/com/formation/cdb/entity/CompanyDto.java
+++ src/main/java/com/formation/cdb/entity/CompanyDto.java
@@ -1,16 +1,13 @@
 package com.formation.cdb.entity;
 
 public class CompanyDto {
-    private long id;
-    
-    private String name;
+    private final long id;
+    private final String name;
 
-    
-    
-    public CompanyDto(long id, String name) {
+    private CompanyDto(CompanyDtoBuilder builder) {
         super();
-        this.id = id;
-        this.name = name;
+        this.id = builder.id;
+        this.name = builder.name;
     }
 
     public long getId() {
@@ -21,5 +18,22 @@ public class CompanyDto {
         return name;
     }
     
+    public static class CompanyDtoBuilder {
+        long id;
+        private final String name;
+        
+        public CompanyDtoBuilder(String name) {
+            this.name = name;
+        }
+        
+        public CompanyDtoBuilder id(long id) {
+            this.id = id;
+            return this;
+        }
+                
+        public CompanyDto build() {
+            return new CompanyDto(this);
+        }
+    }
     
 }
